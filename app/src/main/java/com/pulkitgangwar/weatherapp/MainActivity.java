bf3213dde66d86b8e30ca63ea9f471e1eaa00357
pulkitgangwar <pulkitgangwar2016@gmail.com>
2021-06-11 07:39:04 +0530

    added getweatherinformation method

--- app/src/main/java/com/pulkitgangwar/weatherapp/MainActivity.java
+++ app/src/main/java/com/pulkitgangwar/weatherapp/MainActivity.java
@@ -1,67 +1,110 @@
 package com.pulkitgangwar.weatherapp;
 
 
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.util.Log;
+import android.view.View;
+import android.widget.*;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DecimalFormat;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    EditText cityInput;
+    TextView cityText;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LinearLayout container = findViewById(R.id.container);
-        this.getData(container);
+        ConstraintLayout container = (ConstraintLayout) findViewById(R.id.container);
 
 
+          btn = (Button) findViewById(R.id.container__button);
+          cityInput = (EditText) findViewById(R.id.container__input);
+          cityText = (TextView) findViewById(R.id.container__output);
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("oncreate","inside the onclick");
+                getWeatherInformation();
+
+            }
+        });
     }
 
-    private void getData(LinearLayout container) {
-        String url = "https://jsonplaceholder.typicode.com/posts";
-        RequestQueue queue = Volley.newRequestQueue(this);
-        JsonArrayRequest objectRequest = new JsonArrayRequest(Request.Method.GET, url,null, new Response.Listener<JSONArray>() {
+
+    private void getWeatherInformation() {
+        String apiKey = "d5276286f613266541840ff100599082";
+        String city = cityInput.getText().toString();
+        String url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid="+ apiKey;
+        Log.d("oncreate",url);
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
-            public void onResponse(JSONArray response) {
-
-                JSONArray jsonArray = response;
-                try {
-                    for(int i = 0;i<jsonArray.length();i++){
-                        TextView postTextView = new TextView(getApplicationContext());
-                        postTextView.setTextColor(R.color.white);
-                        JSONObject post = jsonArray.getJSONObject(i);
-                        String title = post.getString("title");
-                        String body = post.getString("body");
-                        postTextView.setText(title);
-                        container.addView(postTextView);
-                    }
+            public void onResponse(JSONObject response) {
+                try{
+                    JSONObject obj = response.getJSONObject("main");
+                    double temp = obj.getDouble("temp");
+                    DecimalFormat df = new DecimalFormat("#,###,##0.00");
+                    double celcius = temp - 273.15;
 
 
-                } catch (Exception err) {
-                    Toast.makeText(getApplicationContext(),err.getMessage(),Toast.LENGTH_SHORT).show();
+                    cityText.setText(String.valueOf( df.format(celcius) + " °C"));
+
+//                    Switch unitSwitch = findViewById(R.id.container__switch);
+//
+//
+//                    if(unitSwitch.isChecked()) {
+//                        DecimalFormat df = new DecimalFormat("#.00");
+//                        double fahrenheit = ((temp - 273.15) * (9/5) + 32);
+//                        cityText.setText(String.valueOf(fahrenheit));
+//                    } else {
+//                        double celcius = temp - 273.15;
+//                        cityText.setText(String.valueOf(celcius));
+//                    }
+
+
+
+                } catch(JSONException err) {
+                    Toast.makeText(getApplicationContext(),err.getMessage().toString(),Toast.LENGTH_SHORT).show();
                 }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError err) {
-                Toast.makeText(getApplicationContext(),err.getMessage(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),err.getMessage().toString(),Toast.LENGTH_SHORT).show();
             }
         });
-
-        queue.add(objectRequest);
+        queue.add(request);
 
     }
+
+
 
 
 
